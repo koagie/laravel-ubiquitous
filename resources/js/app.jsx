e78a24e6410608362b4e37aa5f955abf57caf459
koagie <kogichan.pikapuuu.17.6@gmail.com>
2022-11-09 19:23:07 +0900

    feat add tailwind

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,7 +7,7 @@ import Logo from '../images/instagramLogo.jpg'
 function App() {
     return (
         <>
-            <h1>Hello World</h1>
+            <h1 className="text-3xl font-bold underline">Hello World</h1>
             <img src={Logo} />
             <Counter />
         </>
